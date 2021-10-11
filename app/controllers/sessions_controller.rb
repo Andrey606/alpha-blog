ddc76c4adb7669dee84c25a6e4a109428a8f8868
Andrey606 <andreykuluev96@gmail.com>
2021-10-11 20:54:53 +0300

    open access only for your's articles

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: session_params[:email].downcase)
     if user && user.authenticate(session_params[:password])
       session[:user_id] = user.id
+
       flash[:notice] = "Logged in succesfully"
       redirect_to user
     else
@@ -17,6 +18,9 @@ class SessionsController < ApplicationController
   end
   
   def destroy
+
+    byebug
+
     session[:user_id] = nil
     flash[:notice] = "Logged out"
     redirect_to root_path
