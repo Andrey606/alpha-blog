3dd6651fed23761281b79bcc984b9fd0de00b052
Andrey606 <andreykuluev96@gmail.com>
2021-10-14 19:00:32 +0300

    add tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,35 +1,38 @@
 class CategoriesController < ApplicationController
-
-  before_action :set_category, only: [:show]
-
-  def index
-  end
+  before_action :require_admin, except: [:index, :show]
 
   def new
     @category = Category.new
   end
 
-  def show
-  end
-
   def create
-    @category = Category.new(name: category_params[:name]) # fors equrity
+    @category = Category.new(category_params)
     if @category.save
-      flash[:notice] = "Category was created succesfuly."
-      redirect_to category_path(@category) # checck prefix in route
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
     else
       render 'new'
     end
   end
 
-  private
+  def index
+    @categories = Category.paginate(page: params[:page], per_page: 5)
+  end
 
-  def set_category
+  def show
     @category = Category.find(params[:id])
   end
+  
+  private
 
   def category_params
     params.require(:category).permit(:name)
   end
 
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
+  end
 end
